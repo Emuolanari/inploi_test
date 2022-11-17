63438ad2e64bd15461c9aa3fb8b2a6d6b8efdf57
Emuolanari Ogulu <emuolanariogulu@Emuolanaris-MacBook-Air.local>
2022-11-17 14:38:50 +0000

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,9 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex flex-col justify-between min-h-screen w-full">
+      <Component {...pageProps} />
+    </div>
+  )
 }
