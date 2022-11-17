60a73b4c818da59579965cd673e6f10a723555f3
Emuolanari Ogulu <emuolanariogulu@Emuolanaris-MacBook-Air.local>
2022-11-17 18:25:15 +0000

    job card style

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,4 +1,4 @@
-import { PlusSmIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import { ChevronRightIcon, PlusIcon, XIcon } from '@heroicons/react/outline'
 import { useState } from 'react'
 
 interface Props {
@@ -9,22 +9,43 @@ interface Props {
 
 export const JobCard: React.FC<Props> = ({ description, location, title }) => {
   const [descriptionOpen, setDescriptionOpen] = useState(false)
+
   return (
-    <div className="flex flex-col rounded-md bg-white px-6">
+    <div
+      className="flex flex-col rounded-md bg-white px-6"
+      onClick={() => {
+        if (!descriptionOpen) setDescriptionOpen(true)
+      }}
+    >
       <div className="flex justify-between py-6">
         <p className=" text-base font-bold">{title}</p>
-        <PlusSmIcon className="w-8 h-8" />
+        {descriptionOpen ? (
+          <XIcon
+            className="w-8 h-8"
+            onClick={() => setDescriptionOpen(false)}
+          />
+        ) : (
+          <PlusIcon
+            className="w-8 h-8"
+            onClick={() => setDescriptionOpen(true)}
+          />
+        )}
       </div>
-      <hr />
-      <div className="flex flex-col py-6">
-        <p>{description}</p>
-        <div className="flex justify-between mt-5">
-          <p className="text-gray-400">{location}</p>
-          <button className="flex gap-3 bg-[#111840] text-white px-6 py-2 rounded-md">
-            <span>Apply now</span> <ChevronRightIcon className="w-5 h-5 mt-1" />
-          </button>
-        </div>
-      </div>
+      {descriptionOpen ? (
+        <>
+          <hr />
+          <div className="flex flex-col py-6">
+            <p>{description}</p>
+            <div className="flex justify-between mt-5">
+              <p className="text-gray-400">{location}</p>
+              <button className="flex gap-3 bg-[#111840] items-center text-white px-3 md:px-6 py-2 rounded-md">
+                <span className="text-sm">Apply now</span>{' '}
+                <ChevronRightIcon className="w-5 h-5" />
+              </button>
+            </div>
+          </div>
+        </>
+      ) : null}
     </div>
   )
 }
