95bd4e4c8802648742c0ddef3a79a3f724cd546d
Emuolanari Ogulu <emuolanariogulu@Emuolanaris-MacBook-Air.local>
2022-11-17 18:26:56 +0000

    reposition login button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-r from-[#83D1A6] via-[#86DCCA] to-[#F6f9EB]">
-      <div className="flex justify-end mr-16 mt-10">
-        <button className="bg-primary text-white text-sm py-2 px-6 rounded-md">
+      <div className="relative">
+        <button className="absolute bg-primary text-white text-sm py-2 px-6 rounded-md top-6 right-7">
           Login
         </button>
       </div>
