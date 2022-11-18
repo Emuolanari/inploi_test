2d6e25490a5b986861452749374ac707724340e6
Emuolanari Osah-Ogulu <emuolanari@gmail.com>
2022-11-18 20:39:13 +0000

    move nav to own component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Image from 'next/image'
-import Link from 'next/link'
 import { createContext, useState } from 'react'
+import Nav from '../src/components/Nav'
 
 interface Context {
   setBackground: (value: string) => void
@@ -17,19 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <PageBackgroundContext.Provider value={{ setBackground }}>
       <div className={`flex flex-col min-h-screen ${background}`}>
-        <div className="flex justify-between py-5 px-6">
-          <Link href="/" className="">
-            <Image
-              src="/Paradigmo_Logo.png"
-              alt="logo"
-              height={35}
-              width={35}
-            />
-          </Link>
-          <button className="bg-primary text-white text-sm py-2 px-6 rounded-md">
-            Login
-          </button>
-        </div>
+        <Nav />
         <Component {...pageProps} />
       </div>
     </PageBackgroundContext.Provider>
