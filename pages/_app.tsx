a1ae285dc1a45b82c79f14421189d841c4b08cbf
Emuolanari Osah-Ogulu <emuolanari@gmail.com>
2022-11-19 15:56:23 +0000

    style touches, meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,6 +21,14 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
+        <meta
+          name="description"
+          content="With Paradigmo, you can search millions of jobs online to find the next step in your career"
+        ></meta>
+        <meta
+          name="keywords"
+          content="jobs search, apply, find jobs, remote jobs, paradigmo, employment, work"
+        />
       </Head>
       <div className={`flex flex-col min-h-screen ${background}`}>
         <Nav />
