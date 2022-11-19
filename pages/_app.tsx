5c3438fc27a4e3ffb0afda9dfe0a35f4dfb0cd1b
Emuolanari Osah-Ogulu <emuolanari@gmail.com>
2022-11-19 11:08:00 +0000

    prevent zooming in when changing page on mobile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { createContext, useState } from 'react'
 import Nav from '../src/components/Nav'
+import Head from 'next/head'
 
 interface Context {
   setBackground: (value: string) => void
@@ -15,6 +16,12 @@ export default function App({ Component, pageProps }: AppProps) {
   const [background, setBackground] = useState('')
   return (
     <PageBackgroundContext.Provider value={{ setBackground }}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
       <div className={`flex flex-col min-h-screen ${background}`}>
         <Nav />
         <Component {...pageProps} />
