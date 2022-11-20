ec41ce4cda0d4588ec9f867d73217f4d4aa1f83e
Emuolanari Osah-Ogulu <emuolanari@gmail.com>
2022-11-20 17:05:58 +0000

    cleanup

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Nav: React.FC = () => {
   return (
     <div className="flex justify-between py-8 px-[6%]">
-      <Link href="/" className="">
+      <Link href="/">
         <Image src="/Paradigmo_Logo.png" alt="logo" height={35} width={35} />
       </Link>
       <button className="bg-primary-300 hover:bg-primary text-white text-sm py-2 px-6 rounded-md">
